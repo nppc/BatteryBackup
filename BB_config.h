20ec7d2d51d6d63fa264ba1b8021cef0ae5ace3f
Pavel Palonen <ppalonen@jw.org>
2016-05-16 13:48:45 +0200

    Fix some major bugs...

--- BB_config.h
+++ BB_config.h
@@ -9,8 +9,8 @@
 #define PIN_BEC_A2       A3    // Analog input from voltage divider from Receiver (Arduino) voltage secondary
 
 // Digital pins for Battery MOSFETS
-#define PIN_BATT_M1		6	// Arduino Nano have LED connected to this pin. Useful for debugging
-#define PIN_BATT_M2		3	//
+#define PIN_BATT_M1		13	// Arduino Nano have LED connected to this pin. Useful for debugging
+#define PIN_BATT_M2		12	//
 
 // Voltage dividers for Battery and BEC voltage sensing
 // Battery 1
@@ -27,21 +27,21 @@
 #define BEC_A2_R2       67  //  3.3K (value in Kohms * 10) resistor to the GND
 
 // Internal voltage reference (5v or 1.1v)
-#define VrefBEC			3.280 // value in V
-#define VrefBATT		3.280 // value in V
+#define VrefBEC			3.276 // value in V
+#define VrefBATT		3.276 // value in V
 
 // voltage offsets for fine tuning voltage readings (value will be added to the final voltage reading)
-#define BATT1_V_OFFSET	0.030	// Offset for adjusting final voltage calculation
-#define BATT2_V_OFFSET	0.090	// Offset for adjusting final voltage calculation
-#define BEC1_V_OFFSET	-0.020	// Offset for adjusting final voltage calculation
+#define BATT1_V_OFFSET	0.000	// Offset for adjusting final voltage calculation
+#define BATT2_V_OFFSET	0.070	// Offset for adjusting final voltage calculation
+#define BEC1_V_OFFSET	0.000	// Offset for adjusting final voltage calculation
 #define BEC2_V_OFFSET	0.000	// Offset for adjusting final voltage calculation
 
 // Threshold voltage for switching from the main battery to secondary
-#define BATT_LOW_THRSLD_NORM 	4.5 // 9 volts is the threshold for switching from the main battery to backup after a delay
-#define BATT_LOW_THRSLD_SHORT	3.5 // 8 volts is the threshold for switching from the main battery to backup imideatly
+#define BATT_LOW_THRSLD_NORM 	6.6 // 9 volts is the threshold for switching from the main battery to backup after a delay
+#define BATT_LOW_THRSLD_SHORT	6.3 // 8 volts is the threshold for switching from the main battery to backup imideatly
 #define BATT_LOW_SAFE_TIME 	   2000 // time in ms to allow the main battery to be under the LOW threshold (if Full throttle is applied)
 #define BEC_LOW_THRSLD_BAD      3.5 // 3 volts - is the voltage from BEC showing that something wrong with it. Working BEC should give out 5 volts.
-#define BATT2_DETECT_THRSLD     2.0 // threshold used at initialization step to determine does backup battery is connected.
+#define BATT2_DETECT_THRSLD     1.0 // threshold used at initialization step to determine does backup battery is connected.
 
 // ***** END OF USER SETTINGS ******
 
@@ -50,8 +50,10 @@
 #define state_FIRSTBATT     1 // First battery is connected.
 #define state_SECONDBATT    2 // Second Battery is connected
 #define state_TWOBATT    	3 // Both batteries is connected
-#define state_FAILSAFE   	4 // In case of two batteries setup, failsafe state is detected
-#define state_HWFAILED   	5 // One of the BECs is not working. It means we have BAD hardware.
+#define state_FAILSAFE1   	4 // In case of two batteries setup, failsafe state is detected
+#define state_FAILSAFE2   	5 // In case of two batteries setup, failsafe state is detected
+#define state_HWFAILED   	6 // One of the BECs is not working. It means we have BAD hardware.
+#define state_DONOTHING   	99 // Do nothing state. It is useful to switch here if nwe do not need to do anything anymore. No chance to go out from this state.
 
 #define TURN_MOSFET_1_ON  digitalWrite(PIN_BATT_M1, HIGH);
 #define TURN_MOSFET_1_OFF digitalWrite(PIN_BATT_M1, LOW);
--- Battery_Backup.ino
+++ Battery_Backup.ino
@@ -3,9 +3,7 @@ In case of two batteries setup, BATT1 is the main battery, and BATT2 is the back
 
 In case of one battery setup, BATT2 should be the main battery.
 
-Analog reference. For Battery measurement we are using VCC as reference, but for BEC we use internal 1.1V reference.
-	At the moment analog reference change done simply with loops (to let voltage stabilize). 
-	TODO. Later it should be done with timers to reduce delays in main loop.
+Analog reference. For Battery and BEC measurement we are using VCC as reference. 
 
 */
 
@@ -43,6 +41,7 @@ byte state = state_INIT;
 
 unsigned long time_before_BATT_switch = 0;	// 0 - counter is not started
 unsigned long time_INIT = 0; // Time while waiting for all voltages to settle
+unsigned long time_FAILSAFE; // Time for waiting for BEC2 get voltage and ADC readings are actual
 
 #ifdef DEBUG 
 	unsigned long lastMillisDEBUG;
@@ -54,23 +53,23 @@ void setup()
 #ifdef DEBUG 
 	Serial.begin(57600); 
 	delay(5000);
-	Serial.println(F("*** OXS Battery Backup V1.0 ***"));	// welcome screen
+	Serial.print(millis()); Serial.println(F(" *** OXS Battery Backup V2.0 ***"));	// welcome screen
 #endif 
-	// Backup battery should be connected at the beginning
-	TURN_MOSFET_1_OFF;	// first set state of the pin (enable or disable pullup resistor)
+	// Need to turn on mosfets to check BECs health
+	TURN_MOSFET_1_ON;	// first set state of the pin (enable or disable pullup resistor)
 	TURN_MOSFET_2_ON; // first set state of the pin (enable or disable pullup resistor)
 	pinMode(PIN_BATT_M1, OUTPUT);	// and then set pin direction
 	pinMode(PIN_BATT_M2, OUTPUT);	// and then set pin direction
+	time_INIT = millis(); // Start a timer for counting 500ms after BECs on, to get caps charged :)
 	// discard first readings to let Analog reference voltage to stabilize and to fill smooth array
-	for(byte i=0;i<60;i++){
+	for(byte i=0;i<30;i++){
 		analog_read = digitalSmooth(analogRead(PIN_BATT_A1), batt1_voltage_SmoothArray);
 		analog_read = digitalSmooth(analogRead(PIN_BATT_A2), batt2_voltage_SmoothArray);
 		analog_read = digitalSmooth(analogRead(PIN_BEC_A1), bec1_voltage_SmoothArray);
 		analog_read = digitalSmooth(analogRead(PIN_BEC_A2), bec2_voltage_SmoothArray);
 		delay(1);
 	}	
-	time_INIT = millis(); //Initialize the time counter for initialization step.
-
+	
 #ifdef DEBUG 
 	lastMillisDEBUG = millis();
 #endif 
@@ -113,121 +112,162 @@ void loop()
 
 // Proccess the state of device
 switch (state) {
-	case state_INIT:	// Initialization
-	  // wait half a second to find what setups is detected (Both Batteries connected, or Second Batt is connected - one batt conf)
-	  //if(time_INIT + 500 > millis()) {
-	  	// check what configuration we have and go to next stage
-	  	if((batt1_ok + bec1_ok == 2) && (batt2_ok + bec2_ok == 0)){
-	  		state = state_FIRSTBATT;
+	case state_INIT:	// Initialization, detect what batteries are connected
+		// Delay for 500ms after mosfets ON before continue... but not interrupt main loop
+		if (time_INIT + 500 < millis()) {
 			#ifdef DEBUG 
-				Serial.println(F("Only one Battery is detected..."));
+				Serial.print(millis()); Serial.println(F(" state_INIT"));
 			#endif
-	  	} else if((batt2_ok + bec2_ok == 2) && (batt1_ok + bec1_ok == 0)){
-	  		state = state_SECONDBATT;
-			#ifdef DEBUG 
-				Serial.println(F("Second Battery is detected..."));
-			#endif
-	  	} else if(batt1_ok + batt2_ok + bec1_ok + bec2_ok == 4) {
-	  		state = state_TWOBATT;
-			#ifdef DEBUG 
-				Serial.print(F("TWO Battery setup is detected..."));
-				Serial.print(" Batt 1: "); Serial.print(batt1_voltage); 
-				Serial.print(", Batt 2: "); Serial.print(batt2_voltage); 
-				#ifdef WITHBEC
-					Serial.print(", BEC 1: "); Serial.print(bec1_voltage); 
-					Serial.print(", BEC 2: "); Serial.print(bec2_voltage); 
+		  	// check what configuration we have and go to the next stage
+		  	if((batt1_ok + bec1_ok == 2) && (batt2_ok + bec2_ok == 0)){
+		  		state = state_FIRSTBATT;
+	  			TURN_MOSFET_2_OFF; // turn off unused BEC
+				#ifdef DEBUG 
+					Serial.print(millis()); Serial.print(F(" First Battery is detected...")); Serial.println(batt1_voltage);
 				#endif
-				Serial.println();
-			#endif
-	  	}
-	  	else if(time_INIT + 500 > millis()) {
-	  		state = state_HWFAILED; // One of the BECs is not giving power out
-			#ifdef DEBUG 
-				Serial.println(F("Failure is detected..."));
-			#endif
-	  	}	
-	  //}
+		  	} else if((batt2_ok + bec2_ok == 2) && (batt1_ok + bec1_ok == 0)){
+		  		state = state_SECONDBATT;
+	  			TURN_MOSFET_1_OFF; // turn off unused BEC
+				#ifdef DEBUG 
+					Serial.print(millis()); Serial.print(F(" Second Battery is detected...")); Serial.println(batt2_voltage);
+				#endif
+		  	} else if(batt1_ok + batt2_ok + bec1_ok + bec2_ok == 4) {
+		  		state = state_TWOBATT;
+				#ifdef DEBUG 
+					Serial.print(millis()); Serial.print(F(" TWO Battery setup is detected..."));
+					Serial.print(" Batt 1: "); Serial.print(batt1_voltage); 
+					Serial.print(", Batt 2: "); Serial.print(batt2_voltage); 
+					#ifdef WITHBEC
+						Serial.print(", BEC 1: "); Serial.print(bec1_voltage); 
+						Serial.print(", BEC 2: "); Serial.print(bec2_voltage); 
+					#endif
+					Serial.println();
+				#endif
+		  	}
+		  	else {
+		  		state = state_HWFAILED; // One of the BECs is not giving power out
+				#ifdef DEBUG 
+					Serial.print(millis()); Serial.println(F(" Failure is detected..."));
+				#endif
+		  	}	
+		}
 	  break;
 	case state_FIRSTBATT:	// Only the first battery is connected. This is not allowed configuration, so, we need to wait for second battery connected.
-		// wait in this state until second battery connected...
-		if(batt2_ok + bec2_ok == 2){state = state_TWOBATT;}
-		if(bec1_ok == 0){state = state_HWFAILED;} // One of the BECs is not giving power out
+		// wait in this state until second battery connected. Remember, taht BEC2 will not have any power...
+		TURN_MOSFET_1_OFF; // in this scenario mosfet should be OFF
+		if(batt2_ok == 1){state = state_TWOBATT;} // Backup battery is connected now
+		//if(batt1_ok == 1 && bec1_ok == 0){state = state_HWFAILED;} // BEC1 is not giving power out
 		break;
 	case state_SECONDBATT:	// Second battery is connected. This is one battery configuration. If later will be connected first battery, we will switch to state with two batteries
 		// Should we change a state?
-		if(batt1_ok + bec1_ok == 2){state = state_TWOBATT;}
+		if(batt1_ok == 1){state = state_TWOBATT;}
 		// connect second battery to RC
 		if(IS_MOSFET_2_OFF) {
 			TURN_MOSFET_2_ON; // Turn on second Mosfet
 			#ifdef DEBUG 
-				Serial.println(F("ONE Battery setup. Second Battery is connected."));
+				Serial.print(millis()); Serial.println(F(" ONE Battery setup. Batt2 is connected."));
 			#endif
 		}	
-		if(bec2_ok == 0){state = state_HWFAILED;} // One of the BECs is not giving power out
+		//if(batt2_ok == 1 && bec2_ok == 0){state = state_HWFAILED;} // BEC2 is not giving power out
 		// in this setup we do not have a backup battery, so stay in this mode until first battery is connected or forever... :)
 		break;
-	case state_TWOBATT:	// Second battery is connected. This is one battery configuration. If later will be connected first battery, we will switch to state with two batteries
-		// This is the two battery setup. First battery is the main battery, and second is backup.
+	case state_TWOBATT:	// First and Second battery is connected.
+		// This is the two battery setup. Now first battery is the main battery, and second is backup.
 		//First of all lets connect first battery and disconnect second if it is not already done...
 		if(IS_MOSFET_1_OFF) {
 			TURN_MOSFET_1_ON; // Turn on first Mosfet
 			delay(10);	//delay 10ms just to make sure that everything is connected
 			#ifdef DEBUG 
-				Serial.println(F("TWO Battery setup. First Battery is connected."));
+				Serial.print(millis()); Serial.println(F(" TWO Battery setup. First Battery is connected."));
 			#endif
 		}	
 		if(IS_MOSFET_2_ON) {
 			TURN_MOSFET_2_OFF; // Turn off second Mosfet
 			#ifdef DEBUG 
-				Serial.println(F("TWO Battery setup. Second Battery is disconnected."));
+				Serial.print(millis()); Serial.println(F(" TWO Battery setup. Second Battery is disconnected as it is not needed."));
 			#endif
 		}	
 		// now check for low voltage thresholds
 		// *************** Check is Main Battery is shorted ***************
 		if(batt1_voltage <= BATT_LOW_THRSLD_SHORT) {	// Main battery is shorted, switch imideatly
-			state = state_FAILSAFE;	// change state to failsafe to connect to backup battery
+			state = state_FAILSAFE1;	// change state to failsafe to connect to backup battery
+			#ifdef DEBUG 
+				Serial.print(millis()); Serial.println(F(" TWO Battery setup. Main battery is shorted, switch imideatly."));
+			#endif
 		}
 		// *************** Check is Main Battery is LOW ****************
 		if(batt1_voltage <= BATT_LOW_THRSLD_NORM) {	// Main battery is LOW, switch to secondary batt but after a delay
 			if(time_before_BATT_switch == 0) {
 				time_before_BATT_switch = millis() + BATT_LOW_SAFE_TIME; // start timer countdown
+				#ifdef DEBUG 
+					Serial.print(millis()); Serial.println(F(" TWO Battery setup. Main battery is LOW, start delay counter..."));
+				#endif
 			} else {
 				if(millis() > time_before_BATT_switch) {
-					state = state_FAILSAFE;	// change state to failsafe to connect to backup battery
+					state = state_FAILSAFE1;	// change state to failsafe to connect to backup battery
+					#ifdef DEBUG 
+						Serial.print(millis()); Serial.println(F(" TWO Battery setup. Main battery is LOW, switch after delay."));
+					#endif
 				}
 			}
 		} else {
 			time_before_BATT_switch = 0;	// BATT1 is back normal within timeout
 		}
-		if(bec1_ok == 0){state = state_FAILSAFE;} // First BEC can fail. In this case we have backup battery
-		if(bec2_ok == 0){state = state_HWFAILED;} // Second BEC should not fail. This is BAD!
+		if(bec1_ok == 0){
+			state = state_FAILSAFE1;
+			#ifdef DEBUG 
+				Serial.print(millis()); Serial.println(F(" TWO Battery setup. Main battery BEC is dead."));
+			#endif
+			
+		} // First BEC can fail. In this case we have backup battery
+		//if(bec2_ok == 0){state = state_HWFAILED;} // Second BEC should not fail. This is BAD!
 		break;
-	case state_FAILSAFE:	// Something went wrong with first battery (shorted or runs out of juice)
+	case state_FAILSAFE1:	// Something went wrong with first battery (shorted or runs out of juice)
 		// switch to the backup battery
 		if(IS_MOSFET_2_OFF) {
 			TURN_MOSFET_2_ON; // Turn on second Mosfet
 			delay(10);	//delay 10ms just to make sure that everything is connected
 			#ifdef DEBUG 
-				Serial.println(F("TWO Battery setup. FAILSAFE. Second Battery is connected."));
+				Serial.print(millis()); Serial.println(F(" TWO Battery setup. FAILSAFE1. Trying to switch to the second Battery..."));
 			#endif
 		}	
-		if(IS_MOSFET_1_ON) {
-			TURN_MOSFET_1_OFF; // Turn off first Mosfet
-			#ifdef DEBUG 
-				Serial.println(F("TWO Battery setup. FAILSAFE. First Battery is disconnected."));
-			#endif
-		}	
-		//	stay in this stage forewer. We don't want to switch back to the first battery, even if voltage on Batt1 will recover.
-		// TODO: But what we should do if second BEC or backup batt will totally fail? Should we switch back to Batt1? It seems yes. Should implement later.
-		break;
+		// Before switch off Batt 1 lets check is Backup Battery is operational (both battery and BEC)
+		// mosfet is already 10ms ON, so there is should be some voltage, but lets wait for some time more because of slow ADC and smoothing...
+		state = state_FAILSAFE2;
+		// start the timer for waiting for voltage to stabilize on BEC2
+		time_FAILSAFE = millis();
+		//break;
+	case state_FAILSAFE2:	// Second stage of failsafe, switching Batt1 off if Batt2 is OK.
+		// check the timer
+		if (time_FAILSAFE+100 < millis()) {
+			// now check, can we switch off Batt1...
+			if (batt2_ok + bec2_ok == 2) {
+				if(IS_MOSFET_1_ON) {
+					TURN_MOSFET_1_OFF; // Turn off first Mosfet
+					#ifdef DEBUG 
+						Serial.print(millis()); Serial.println(F(" TWO Battery setup. FAILSAFE2. First Battery is disconnected."));
+					#endif
+				}	
+			} else {
+				Serial.print(millis()); Serial.println(F(" TWO Battery setup. FAILSAFE2. Backup battery is dead. Can't switch to it."));
+				TURN_MOSFET_2_OFF; // stay on the first battery
+				// stay forewer here
+				state = state_DONOTHING; // TODO later add to the config
+			}
+		}
+		break;	
 	case state_HWFAILED:	// Worsest case - our hardware is failed.
 		// TODO: should we blink with LEDs or what we should do?
 			#ifdef DEBUG 
-				Serial.println(F("HARDWARE FAILED. CHECK YOUR BECs!"));	// dead screen
-				delay(500);
+				Serial.print(millis()); Serial.println(F(" HARDWARE FAILED. CHECK YOUR BECs!"));	// dead screen
+				//delay(500);
 				state = state_INIT;	// try to detect setup again...
+				TURN_MOSFET_1_ON;	// first set state of the pin (enable or disable pullup resistor)
+				TURN_MOSFET_2_ON; // first set state of the pin (enable or disable pullup resistor)
 				time_INIT = millis(); //Initialize the time counter for initialization step.
 			#endif
+		break;
+	case state_DONOTHING:
 		break;
 }
 
@@ -235,7 +275,8 @@ switch (state) {
 #ifdef DEBUG 
 	if (millis() > lastMillisDEBUG + 1000) { 
         lastMillisDEBUG = millis() ;  
-		Serial.print("Batt 1: "); Serial.print(batt1_voltage); 
+        Serial.print(millis());
+		Serial.print(" Batt 1: "); Serial.print(batt1_voltage); 
 		Serial.print("   Batt 2: "); Serial.print(batt2_voltage); 
 		#ifdef WITHBEC
 			Serial.print("   BEC 1: "); Serial.print(bec1_voltage); 
